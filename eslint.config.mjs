2adaae2ddc5f923cab62075c133c18581ff3ecd5
Amy Lau <tszyanalau@gmail.com>
2025-01-27 15:50:34 +0900

    chore: change eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,99 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+    rules: {
+      'array-callback-return': 'error',
+      'brace-style': [
+        'error',
+        '1tbs',
+        {
+          allowSingleLine: true,
+        },
+      ],
+      'comma-dangle': [
+        'error',
+        {
+          arrays: 'always-multiline',
+          objects: 'always-multiline',
+          imports: 'always-multiline',
+          exports: 'always-multiline',
+          functions: 'never',
+        },
+      ],
+      'dot-notation': 'error',
+      indent: 0,
+      'max-len': [
+        'error',
+        {
+          code: 130,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreComments: true,
+        },
+      ],
+      'multiline-ternary': 0,
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error'],
+        },
+      ],
+      'no-else-return': [
+        'error',
+        {
+          allowElseIf: false,
+        },
+      ],
+      'no-lonely-if': 'error',
+      'no-multi-assign': 'error',
+      'no-negated-condition': 'error',
+      'no-param-reassign': 'error',
+      'no-return-assign': 'error',
+      'no-undef': 0,
+      'no-undef-init': 'error',
+      'no-unneeded-ternary': 'error',
+      'no-unused-expressions': 'error',
+      'object-shorthand': 'error',
+      'operator-linebreak': 0,
+      'space-in-parens': 0,
+      'prefer-arrow-callback': 'error',
+      'prefer-destructuring': [
+        'error',
+        {
+          object: true,
+        },
+      ],
+      'prefer-regex-literals': 'error',
+      'prefer-template': 'error',
+      'quote-props': ['error', 'as-needed'],
+      quotes: [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+        },
+      ],
 
-export default eslintConfig;
+      'react/button-has-type': 'error',
+      'react/jsx-boolean-value': 'error',
+      'react/jsx-no-useless-fragment': 'error',
+      'react/no-array-index-key': 'error',
+      'react/react-in-jsx-scope': 0,
+      'react/self-closing-comp': 'error',
+      'react/state-in-constructor': 'error',
+    },
+  }),
+]
+
+export default eslintConfig
